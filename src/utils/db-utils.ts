672c09c892dc7d7a845759cb3b6e3fc9728e8302
Alex Schreffler <aschreff53@icloud.com>
2023-04-08 14:13:04 -0400

    method for testdb

--- src/utils/db-utils.ts
+++ src/utils/db-utils.ts
@@ -7,13 +7,30 @@ let Config = require('../../config/config.json');
 export class DatabaseUtils {
     public static async connectDB(): Promise<void> {
         try {
+            const name = 'refy';
             await mongoose.connect(Config.mongoUri, {
+                dbName: name,
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
             } as ConnectOptions);
-            console.log(`Connected to MongoDB at ${Config.mongoUri}`);
+            console.log(`Connected to MongoDB at ${Config.mongoUri} and using database ${name}`);
         } catch (error) {
             console.error(error);
+            process.exit(1);
+        }
+    }
+
+    public static async connectDBForTesting(): Promise<void> {
+        try {
+            const name = 'test';
+            await mongoose.connect(Config.mongoUri, {
+                dbName: name,
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            } as ConnectOptions);
+            console.log(`Connected to MongoDB at ${Config.mongoUri} and using database ${name}`);
+        } catch (error) {
+            console.log(error);
             process.exit(1);
         }
     }
